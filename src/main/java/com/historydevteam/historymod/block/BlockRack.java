67894bb4e61840b1dd30bce7b9a870fd212bbb56
Technic <benjamin@qbrundage.com>
2019-05-21 20:07:51 -0700

    Update BlockRack.java

--- src/main/java/com/historydevteam/historymod/block/BlockRack.java
+++ src/main/java/com/historydevteam/historymod/block/BlockRack.java
@@ -1,7 +1,20 @@
 package com.historydevteam.historymod.block;
 
-import net.minecraft.item.ItemBlock;
+import com.historydevteam.historymod.util.Reference;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 
-public interface ISpecialItemBlock {
-  ItemBlock getItemBlock();
+import java.util.Random;
+
+public class BlockRack extends Block {
+
+  public BlockFlintOre() {
+    super(Material.ROCK);
+    setTranslationKey(Reference.MOD_ID + ".rack");
+    setRegistryName(Reference.MOD_ID, "rack");
+    setCreativeTab(Reference.HISTORY_CREATIVE_TAB);
+  }
 }
